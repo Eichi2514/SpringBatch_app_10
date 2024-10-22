b1de01424e8525331048aeac0a6352dd906bbb19
Eichi2514 <picon1317@gmail.com>
2024-10-22 15:12:07 +0900

    셋팅

---
+++ src/main/java/com/koreait/exam/springvatch_app_10/SpringVatchApp10Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.springvatch_app_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVatchApp10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVatchApp10Application.class, args);
+    }
+
+}
